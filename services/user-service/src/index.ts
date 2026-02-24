48cb538a0e719919f04d6ccd828482350b80c5d4
nick <120076510+nick-0037@users.noreply.github.com>
2026-02-24 17:52:47 -0400

    feat(user-service): add entry point and express server

---
+++ services/user-service/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`User service is running on port ${PORT}`);
+});
