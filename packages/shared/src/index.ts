392c960c59e4af19078338844d978217670f46a8
nick <120076510+nick-0037@users.noreply.github.com>
2026-03-03 21:17:48 -0400

    feat(shared): add professional logging utilities

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,4 +2,6 @@ export * from "./middlewares/validate";
 export * from "./middlewares/auth.middleware";
 export * from "./middlewares/error.handler";
 export * from "./utils/errors";
+export * from "./utils/logger";
 export * from "./consul";
+export * from "./middlewares/logger.middleware";
